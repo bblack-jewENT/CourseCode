395c08f6996415611588dbd31385c95eca225b3b
Moses Rametsi <158572657+bblack-jewENT@users.noreply.github.com>
2025-11-11 16:10:54 +0200

    Update Footer.jsx
    Add company name on footer.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ const Footer = () => {
   return (
     <footer className="footer">
       <div className="container">
-        <p>&copy; 2025 CodeLearn. All rights reserved.</p>
+        <p>&copy; 2025 CodeLearn | bblackjew ENTj. All rights reserved.</p>
         <div>
           <a href="#">Privacy Policy</a>
           <a href="#">Terms of Service</a>
