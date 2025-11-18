80861b513a46d793d307cd2c94f08bd3cd332227
Moses Rametsi <158572657+bblack-jewENT@users.noreply.github.com>
2025-11-18 21:54:55 +0200

    Fix the navbar to fit nice.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,9 +3,11 @@ import { Link } from "react-router-dom";
 const Navbar = () => {
   return (
     <nav className="navbar">
+      <Link to="/" className="logo">
+        CodeLearn
+      </Link>
       <div className="container">
-        <Link to="/">CodeLearn</Link>
-        <div>
+        <div style={{ marginRight: "35px" }}>
           <Link to="/">Home</Link>
           <Link to="/courses">Courses</Link>
           <Link to="/dashboard">Dashboard</Link>
